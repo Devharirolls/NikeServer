3bf772b56d8eedb7ae6dfa7455a05257509ce5fe
Devharirolls <138101854+Devharirolls@users.noreply.github.com>
2023-06-29 13:35:29 +0530

    Update products.js

--- src/database/products.js
+++ src/database/products.js
@@ -15,21 +15,3 @@ module.exports = {
 };
 
 
-// const { ObjectId } = require('mongodb');
-// const db = require('./db');
-
-// const getAllProducts = async () =>{
-//     return await db.products.find().toArray();
-
-// };
-
-// const getProduct = async(id) =>{
-//     return await db.products.findOne({_id: new ObjectId(id)});
-    
-// };
-
-
-// module.exports={
-//     getAllProducts,
-//     getProduct,
-// };
