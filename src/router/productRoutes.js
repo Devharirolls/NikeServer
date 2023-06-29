ac7717c2f1bc466e98e7cdbc8fddfaeba6eae27f
Devharirolls <138101854+Devharirolls@users.noreply.github.com>
2023-06-29 13:36:16 +0530

    Update productRoutes.js

--- src/router/productRoutes.js
+++ src/router/productRoutes.js
@@ -25,32 +25,3 @@ router.get('/:productId', async (req, res) => {
 module.exports = router;
 
 
-
-// const express =require('express');
-// const router = express.Router();
-// const {getAllProducts,getProduct} = require('../database/products')
-
-// router.get('/', async (req,res) =>{
-//     const products = await getAllProducts();
-//     console.log(products);
-//     res.send({status:'ok',data:products});
-// });
-
-// router.get('/:productId', async (req,res)=>{
-//     try {
-//         const Product = await getProduct(req.params.productId)
-    
-//         if(!Product){
-//             res.status(404).send({status:'Failed' , error : 'Product not found'})
-//             return;
-//         }
-
-//         res.send({status:'ok',data: Product});
-
-
-//     } catch (e) {
-//         res.status(401).send({status:'FAILED',error:e.message});
-//     }
-// })
-
-// module.exports = router;
