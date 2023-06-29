8a8b4e819abbcafb53978ad909edb4ba6e4c971c
Devharirolls <138101854+Devharirolls@users.noreply.github.com>
2023-06-29 13:35:09 +0530

    Update orders.js

--- src/database/orders.js
+++ src/database/orders.js
@@ -15,19 +15,3 @@ module.exports = {
 };
 
 
-// const db =require('./db');
-
-// //db.orders
-// const getOrder = async (ref) =>{
-//     return await db.orders.findOne({ref})
-// }
-
-// const createOrder = async (order)=>{
-//    const result = await db.orders.insertOne(order);
-//    return {...order, _id: result.insertedId };
-// };
-
-// module.exports={
-//     getOrder,
-//     createOrder,
-// };
